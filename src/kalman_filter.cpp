7d57c90d5b462c73f63e16e15e8f6fc447ec87b9
Unknown <elias@zimi.de>
2018-03-10 06:19:31 +0100

    Apply google coding conventions

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -5,13 +5,14 @@ using namespace std;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-
 // Please note that the Eigen library does not initialize 
 // VectorXd or MatrixXd objects with zeros upon creation.
 
-KalmanFilter::KalmanFilter() {}
+KalmanFilter::KalmanFilter() {
+}
 
-KalmanFilter::~KalmanFilter() {}
+KalmanFilter::~KalmanFilter() {
+}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
@@ -24,63 +25,60 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-    x_ = F_ * x_;
-    MatrixXd Ft = F_.transpose();
-    P_ = F_ * P_ * Ft + Q_;
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-    /**
-      * update the state by using Kalman Filter equations
-    */
-    VectorXd z_pred = H_ * x_;
-    VectorXd y = z - z_pred; /* error function */
-    MatrixXd Ht = H_.transpose();
-    MatrixXd S = H_ * P_ * Ht + R_;
-    MatrixXd Si = S.inverse();
-    MatrixXd PHt = P_ * Ht;
-    MatrixXd K = PHt * Si;
+  /**
+   * update the state by using Kalman Filter equations
+   */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred; /* error function */
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
 
-    //new estimate
-    x_ = x_ + (K * y);
-    long x_size = x_.size();
-    MatrixXd I = MatrixXd::Identity(x_size, x_size);
-    P_ = (I - K * H_) * P_;
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   // Recalculate x object state to rho, phi, rho_dot coordinates
-    double rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
-    if(rho <= 0.0001)
-    {
-      return;
-    }
-    double phi = atan2(x_(1) , x_(0));
-    double rho_dot = (x_(0)*x_(2) + x_(1)*x_(3)) / rho;
+  double rho = sqrt(x_(0) * x_(0) + x_(1) * x_(1));
+  if (rho <= 0.0001) {
+    return;
+  }
+  double phi = atan2(x_(1), x_(0));
+  double rho_dot = (x_(0) * x_(2) + x_(1) * x_(3)) / rho;
 
-    VectorXd h = VectorXd(3); // h(x_)
-    h << rho, phi, rho_dot;
+  VectorXd h = VectorXd(3);  // h(x_)
+  h << rho, phi, rho_dot;
 
-    VectorXd y = z - h;
+  VectorXd y = z - h;
 
-    while(y[1] > M_PI)
-    {
-      y[1]  -= (2*M_PI);
-    }
-    while(y[1]  < (-M_PI))
-    {
-      y[1]  += (2*M_PI);
-    }
-    // Calculations are essentially the same to the Update function
-    MatrixXd Ht = H_.transpose();
-    MatrixXd S = H_ * P_ * Ht + R_;
-    MatrixXd Si = S.inverse();
-    MatrixXd PHt = P_ * Ht;
-    MatrixXd K = PHt * Si;
+  while (y[1] > M_PI) {
+    y[1] -= (2 * M_PI);
+  }
+  while (y[1] < (-M_PI)) {
+    y[1] += (2 * M_PI);
+  }
+  // Calculations are essentially the same to the Update function
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
 
-    //new estimate
-    x_ = x_ + (K * y);
-    long x_size = x_.size();
-    MatrixXd I = MatrixXd::Identity(x_size, x_size);
-    P_ = (I - K * H_) * P_;
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
